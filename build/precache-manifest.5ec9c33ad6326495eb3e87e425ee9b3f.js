5d773725010a502a17f6552115995775e0fa85c0
Robee Ghosh <rob93ghosh@gmail.com>
2020-11-23 15:01:53 +0400

    Updates

---
+++ build/precache-manifest.5ec9c33ad6326495eb3e87e425ee9b3f.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3416a3c2c2f52095526c0ac378639a87",
+    "url": "/robster619007/PhotoGallery/index.html"
+  },
+  {
+    "revision": "1ad6b731964ff08a9cea",
+    "url": "/robster619007/PhotoGallery/static/css/6.3614eac3.chunk.css"
+  },
+  {
+    "revision": "889507e589ca8d664b1b",
+    "url": "/robster619007/PhotoGallery/static/css/main.34b609fb.chunk.css"
+  },
+  {
+    "revision": "df09b2f40d27d153082f",
+    "url": "/robster619007/PhotoGallery/static/js/0.afb14f99.chunk.js"
+  },
+  {
+    "revision": "0b21eabba5b6599ffa49",
+    "url": "/robster619007/PhotoGallery/static/js/10.3416703f.chunk.js"
+  },
+  {
+    "revision": "7ea6fa04a5655403fe99",
+    "url": "/robster619007/PhotoGallery/static/js/11.33045585.chunk.js"
+  },
+  {
+    "revision": "477cfcbe53a3ca740a72",
+    "url": "/robster619007/PhotoGallery/static/js/12.4a9641c3.chunk.js"
+  },
+  {
+    "revision": "7938be80ad1ace2ef694",
+    "url": "/robster619007/PhotoGallery/static/js/13.7c5b9b2f.chunk.js"
+  },
+  {
+    "revision": "64723f5720d46ec48d43",
+    "url": "/robster619007/PhotoGallery/static/js/14.a5a6bb50.chunk.js"
+  },
+  {
+    "revision": "47b782c8c7e2a316385c",
+    "url": "/robster619007/PhotoGallery/static/js/15.461d6a2f.chunk.js"
+  },
+  {
+    "revision": "097a288b15e3bbb236c6",
+    "url": "/robster619007/PhotoGallery/static/js/16.454e6931.chunk.js"
+  },
+  {
+    "revision": "cd51244cf88462fd8214",
+    "url": "/robster619007/PhotoGallery/static/js/17.4b5abc1d.chunk.js"
+  },
+  {
+    "revision": "008d93b3a80126fc7e5f",
+    "url": "/robster619007/PhotoGallery/static/js/18.9230ff1d.chunk.js"
+  },
+  {
+    "revision": "c4f5da5c35aaabb34e87",
+    "url": "/robster619007/PhotoGallery/static/js/19.f525f471.chunk.js"
+  },
+  {
+    "revision": "70f7ec265960adfb5a43",
+    "url": "/robster619007/PhotoGallery/static/js/20.ae35fb92.chunk.js"
+  },
+  {
+    "revision": "fc2108aafcfe90d3d2b9",
+    "url": "/robster619007/PhotoGallery/static/js/21.c66b3eda.chunk.js"
+  },
+  {
+    "revision": "d4ae66f3354c0ab000b2",
+    "url": "/robster619007/PhotoGallery/static/js/22.d83d1235.chunk.js"
+  },
+  {
+    "revision": "0562e099478eae1f92e7",
+    "url": "/robster619007/PhotoGallery/static/js/23.e763db91.chunk.js"
+  },
+  {
+    "revision": "bf7001b35e8eb6ebdd91",
+    "url": "/robster619007/PhotoGallery/static/js/24.4b878c88.chunk.js"
+  },
+  {
+    "revision": "739eb56a94acfc3c5fcf",
+    "url": "/robster619007/PhotoGallery/static/js/25.be2e28c9.chunk.js"
+  },
+  {
+    "revision": "f45739d23f128adc8894",
+    "url": "/robster619007/PhotoGallery/static/js/26.d42ad55a.chunk.js"
+  },
+  {
+    "revision": "837a2c5c10ef2d46b136",
+    "url": "/robster619007/PhotoGallery/static/js/27.5639a457.chunk.js"
+  },
+  {
+    "revision": "d97ad2bfff159318d5ac",
+    "url": "/robster619007/PhotoGallery/static/js/28.a1f78c8a.chunk.js"
+  },
+  {
+    "revision": "89c41f40491d19f0d878",
+    "url": "/robster619007/PhotoGallery/static/js/29.e3a7be62.chunk.js"
+  },
+  {
+    "revision": "f9118f5786ec186b0ede",
+    "url": "/robster619007/PhotoGallery/static/js/30.147be3f1.chunk.js"
+  },
+  {
+    "revision": "cb1346944306115816f9",
+    "url": "/robster619007/PhotoGallery/static/js/31.0d935da8.chunk.js"
+  },
+  {
+    "revision": "fcb77024862a8e05a725",
+    "url": "/robster619007/PhotoGallery/static/js/32.7dffd444.chunk.js"
+  },
+  {
+    "revision": "44c1819ee9bf162f19e1",
+    "url": "/robster619007/PhotoGallery/static/js/33.dd8c0ba6.chunk.js"
+  },
+  {
+    "revision": "594a3eee7e1a9aac168b",
+    "url": "/robster619007/PhotoGallery/static/js/34.f08e403a.chunk.js"
+  },
+  {
+    "revision": "62fc366e8859b68bd318",
+    "url": "/robster619007/PhotoGallery/static/js/35.fe8d4819.chunk.js"
+  },
+  {
+    "revision": "3fcb4ad2c6b916fcf0c8",
+    "url": "/robster619007/PhotoGallery/static/js/36.f3e4d382.chunk.js"
+  },
+  {
+    "revision": "0333ed26a9b95263dea5",
+    "url": "/robster619007/PhotoGallery/static/js/37.36dd4ee5.chunk.js"
+  },
+  {
+    "revision": "24bea03bce043ee83b18",
+    "url": "/robster619007/PhotoGallery/static/js/38.936e651e.chunk.js"
+  },
+  {
+    "revision": "5ab52cd6198d0fdc3deb",
+    "url": "/robster619007/PhotoGallery/static/js/39.5ef8f52a.chunk.js"
+  },
+  {
+    "revision": "18551ea9e2515a794e06",
+    "url": "/robster619007/PhotoGallery/static/js/40.561fdc62.chunk.js"
+  },
+  {
+    "revision": "92068394ad8252905340",
+    "url": "/robster619007/PhotoGallery/static/js/41.b30a30df.chunk.js"
+  },
+  {
+    "revision": "ea92514af02c5e2dffaa",
+    "url": "/robster619007/PhotoGallery/static/js/42.45ebce33.chunk.js"
+  },
+  {
+    "revision": "f99cddd19b0d5292f7e0",
+    "url": "/robster619007/PhotoGallery/static/js/43.ab268f4b.chunk.js"
+  },
+  {
+    "revision": "b2c9eff57bf3d474250f",
+    "url": "/robster619007/PhotoGallery/static/js/44.2d9af3fa.chunk.js"
+  },
+  {
+    "revision": "17f96fae053be0ddc280",
+    "url": "/robster619007/PhotoGallery/static/js/45.2027be9b.chunk.js"
+  },
+  {
+    "revision": "86df35684c66260dc381",
+    "url": "/robster619007/PhotoGallery/static/js/46.93537363.chunk.js"
+  },
+  {
+    "revision": "8de723a564013722d42b",
+    "url": "/robster619007/PhotoGallery/static/js/47.fd181b03.chunk.js"
+  },
+  {
+    "revision": "a2440b48a13410c78fec",
+    "url": "/robster619007/PhotoGallery/static/js/48.4527354e.chunk.js"
+  },
+  {
+    "revision": "298afc1487e4e48078a1",
+    "url": "/robster619007/PhotoGallery/static/js/49.20cc9b0f.chunk.js"
+  },
+  {
+    "revision": "08eb29cc6337fe407097",
+    "url": "/robster619007/PhotoGallery/static/js/50.3e8d1425.chunk.js"
+  },
+  {
+    "revision": "04cc6d4e7dda3b3507ac",
+    "url": "/robster619007/PhotoGallery/static/js/51.5a03de8a.chunk.js"
+  },
+  {
+    "revision": "5515914abbcd01c90282",
+    "url": "/robster619007/PhotoGallery/static/js/52.088191a7.chunk.js"
+  },
+  {
+    "revision": "ef9999b813dc95d17601",
+    "url": "/robster619007/PhotoGallery/static/js/53.121f48c7.chunk.js"
+  },
+  {
+    "revision": "aaf11ed7363e747b7da2",
+    "url": "/robster619007/PhotoGallery/static/js/54.a0c82a08.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/robster619007/PhotoGallery/static/js/54.a0c82a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f08c27ed02f271a5aa9",
+    "url": "/robster619007/PhotoGallery/static/js/55.a9832573.chunk.js"
+  },
+  {
+    "revision": "1d78dfdfb2f64185903b",
+    "url": "/robster619007/PhotoGallery/static/js/56.6f0a05fa.chunk.js"
+  },
+  {
+    "revision": "0af5c4cbefb513f03838",
+    "url": "/robster619007/PhotoGallery/static/js/57.75ffabd9.chunk.js"
+  },
+  {
+    "revision": "7aba1e626cb0eef30ef3",
+    "url": "/robster619007/PhotoGallery/static/js/58.381749ab.chunk.js"
+  },
+  {
+    "revision": "320d5db444d829c51585",
+    "url": "/robster619007/PhotoGallery/static/js/59.1c52a444.chunk.js"
+  },
+  {
+    "revision": "1ad6b731964ff08a9cea",
+    "url": "/robster619007/PhotoGallery/static/js/6.fb7718c7.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/robster619007/PhotoGallery/static/js/6.fb7718c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6056c6a686cc8dd8f8b",
+    "url": "/robster619007/PhotoGallery/static/js/60.0325040b.chunk.js"
+  },
+  {
+    "revision": "74700e54ad9d7f7d17a2",
+    "url": "/robster619007/PhotoGallery/static/js/61.d3d74e2f.chunk.js"
+  },
+  {
+    "revision": "07169dc008c26b0cd63c",
+    "url": "/robster619007/PhotoGallery/static/js/62.75a4ba8f.chunk.js"
+  },
+  {
+    "revision": "7b17086dde872a9368e4",
+    "url": "/robster619007/PhotoGallery/static/js/63.de6a4427.chunk.js"
+  },
+  {
+    "revision": "8c6a9c48e52e7fe6329a",
+    "url": "/robster619007/PhotoGallery/static/js/64.d9ba538a.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/robster619007/PhotoGallery/static/js/64.d9ba538a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199c91fe938979eacd8c",
+    "url": "/robster619007/PhotoGallery/static/js/65.cd7538d9.chunk.js"
+  },
+  {
+    "revision": "31433a390a5d252cd9f5",
+    "url": "/robster619007/PhotoGallery/static/js/66.ce5d2745.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/robster619007/PhotoGallery/static/js/66.ce5d2745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4628d1f50d3c295cdccb",
+    "url": "/robster619007/PhotoGallery/static/js/67.db2cf330.chunk.js"
+  },
+  {
+    "revision": "e6eb67f8bd35a0dd68d4",
+    "url": "/robster619007/PhotoGallery/static/js/68.2f174084.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/robster619007/PhotoGallery/static/js/68.2f174084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5afe9ad1b2b61ba39774",
+    "url": "/robster619007/PhotoGallery/static/js/7.40541efe.chunk.js"
+  },
+  {
+    "revision": "7307e25427b05a9e4e0a",
+    "url": "/robster619007/PhotoGallery/static/js/8.e70ce59a.chunk.js"
+  },
+  {
+    "revision": "728c47c56b001a16be5e",
+    "url": "/robster619007/PhotoGallery/static/js/9.d129b0ce.chunk.js"
+  },
+  {
+    "revision": "889507e589ca8d664b1b",
+    "url": "/robster619007/PhotoGallery/static/js/main.ec87fc5c.chunk.js"
+  },
+  {
+    "revision": "c14704e3c93fe36514c7",
+    "url": "/robster619007/PhotoGallery/static/js/polyfills-dom.8d63b17a.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/robster619007/PhotoGallery/static/js/polyfills-dom.8d63b17a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2193a00432b127246d25",
+    "url": "/robster619007/PhotoGallery/static/js/runtime-main.68aba377.js"
+  },
+  {
+    "revision": "17ce7bdf38498099fa3d",
+    "url": "/robster619007/PhotoGallery/static/js/stencil-polyfills-css-shim.03c5367b.chunk.js"
+  },
+  {
+    "revision": "7d426847c5f62cbc6be6",
+    "url": "/robster619007/PhotoGallery/static/js/stencil-polyfills-dom.263e0fdf.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/robster619007/PhotoGallery/static/js/stencil-polyfills-dom.263e0fdf.chunk.js.LICENSE.txt"
+  }
+]);
---
+++ build/static/js/55.a9832573.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpphoto-gallary"]=this["webpackJsonpphoto-gallary"]||[]).push([[55],{152:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(12),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.h)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then((function(){Object(e.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=55.a9832573.chunk.js.map
